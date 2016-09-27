d4bba7deb99ba895ef6235ee2a3f105c77977846
printer <printer@printers-iMac.local>
2016-09-27 17:53:35 +0800

    dsds

---
+++ HSKExamination/HSKExamination/Model/SingleChoice.m
@@ -0,0 +1,13 @@
+//
+//  SingleChoice.m
+//  HSKExamination
+//
+//  Created by printer on 9/27/16.
+//  Copyright © 2016 printer. All rights reserved.
+//
+
+#import "SingleChoice.h"
+
+@implementation SingleChoice
+
+@end
