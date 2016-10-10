229e4c71c462c7b0dc4a1c6eabb6e85f8b5ea3c0
printer <printer@printers-iMac.local>
2016-10-10 17:30:11 +0800

    dsds

---
+++ HSKExamination/HSKExamination/未命名文件夹/level4/SingleChoice4.h
@@ -0,0 +1,13 @@
+//
+//  SingleChoice4.h
+//  HSKExamination
+//
+//  Created by printer on 2016/10/10.
+//  Copyright © 2016年 printer. All rights reserved.
+//
+
+#import "SingleChoice.h"
+
+@interface SingleChoice4 : SingleChoice
+
+@end
---
+++ HSKExamination/HSKExamination/未命名文件夹/level4/SingleChoice4.m
@@ -0,0 +1,13 @@
+//
+//  SingleChoice4.m
+//  HSKExamination
+//
+//  Created by printer on 2016/10/10.
+//  Copyright © 2016年 printer. All rights reserved.
+//
+
+#import "SingleChoice4.h"
+
+@implementation SingleChoice4
+
+@end
