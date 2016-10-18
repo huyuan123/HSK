2faf8979502160793d7c1f1a6a3669f84687a8cd
hiddy <you@example.com>
2016-10-19 06:55:56 +0800

    dsds

---
+++ HSKExamination/HSKExamination/Controller/StatisticalCenterController.h
@@ -0,0 +1,13 @@
+//
+//  StatisticalCenterController.h
+//  HSKExamination
+//
+//  Created by hiddy on 16/10/18.
+//  Copyright © 2016年 printer. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface StatisticalCenterController : BaseViewController
+
+@end
